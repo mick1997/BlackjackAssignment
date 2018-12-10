e7a329f1e8d1f878af643d15b4deee3ac29a972a
jackfeng <mickjack2013@gmail.com>
2018-12-09 22:23:47 -0500

    Game runs in indefinite loops due to gameOver in GameController is always false

--- src/blackjack/mainEntry/mainDummy.java
+++ src/blackjack/mainEntry/mainDummy.java
@@ -1,7 +1,84 @@
 package blackjack.mainEntry;
 
+import blackjack.card.Card;
+import blackjack.controller.GameController;
+import blackjack.controller.ICardProvider;
+import blackjack.controller.IGamePolicy;
+import blackjack.controller.IPlayer;
+
+class DummyGamePolicy implements IGamePolicy {
+
+    @Override
+    public boolean isPlayerWin(IPlayer dealer, IPlayer player) {
+        return false;
+    }
+}
+
+class DummyCardProvider implements ICardProvider {
+
+    @Override
+    public Card[] getNext(int count) {
+        return new Card[0];
+    }
+}
+
+class DummyPlayer implements IPlayer {
+
+    private String name;
+
+    public DummyPlayer(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void acceptCards(Card[] newCards) {
+
+    }
+
+    @Override
+    public void think() {
+        System.out.println(String.format("Player %s thinking", name));
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public boolean isBlackJack() {
+        return false;
+    }
+
+    @Override
+    public boolean isBusted() {
+        return false;
+    }
+
+    @Override
+    public void addCredit(int amount) {
+
+    }
+
+    @Override
+    public int getBet() {
+        return 0;
+    }
+}
+
+
 public class mainDummy {
     public static void main(String[] args) {
+        simpleDummyOneGame();
         System.out.println("It works");
+    }
+
+    static void simpleDummyOneGame() {
+        IPlayer[] players = new IPlayer[]{new DummyPlayer("#1"), new DummyPlayer("#2")};
+        GameController controller = new GameController(
+                new DummyGamePolicy(), new DummyCardProvider(), new DummyPlayer("Dealer"), players
+        );
+        controller.run();
+
     }
 }
